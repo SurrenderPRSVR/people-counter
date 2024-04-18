cb3bffc93f0c6a26dcd9af1adcc91aa0d9b671d6
Heather <hflanders.prsvr@gmail.com>
2024-04-17 20:01:53 -0500

    inprogress

---
+++ script.js
@@ -0,0 +1,24 @@
+// document.getElementById("count-el").innerText = 5
+
+//let firstBatch = 5
+//let secondBatch = 7
+//let count = firstBatch + secondBatch
+//console.log(count)
+
+
+//let myAge = 38
+//let humanDogRatio = 7
+//let myDogAge = myAge * humanDogRatio
+//console.log(myDogAge)
+
+
+//let count = 5
+//count = count + 1 // = 6
+//console.log(count)
+
+//let bonusPoints = 50
+//bonusPoints = bonusPoints + 50
+//bonusPoints = bonusPoints - 75
+//bonusPoints = bonusPoints + 45
+//console.log(bonusPoints)
+
