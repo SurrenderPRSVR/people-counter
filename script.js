03e3a4b1f370b3654a70bc1004acce5e3c3998b3
Heather <hflanders.prsvr@gmail.com>
2024-04-18 19:52:26 -0500

    InProgress

--- script.js
+++ script.js
@@ -16,9 +16,67 @@
 //count = count + 1 // = 6
 //console.log(count)
 
-//let bonusPoints = 50
-//bonusPoints = bonusPoints + 50
-//bonusPoints = bonusPoints - 75
-//bonusPoints = bonusPoints + 45
-//console.log(bonusPoints)
+
+
+// intialize the count as 0
+// listen for clicks on the increment button
+// incremement the count variable when the button is clicked
+// change the count-el in the HTML to reflect the new count
+
+let count = 0
+
+function incrementCount() {
+    
+    count = count + 1
+    console.log(count)
+
+}
+
+incrementCount()
+
+
+
+
+//function increment() {
+    //console.log("The button was clicked")
+//}
+
+
+//create a function that logs out the number 42 to the console
+
+//function log42() {
+    //console.log(42);
+//}
+
+//log42();
+
+//ceate a function that logs out the sum of all the lap times
+
+//let lap1 = 34
+//let lap2 = 33
+//let lap3 = 36
+
+//function logLapTime() {
+    //console.log(lap1 + lap2 + lap3)
+    
+//}
+
+//logLapTime()
+
+
+//create a function that increments the lapsCompleted variable with one
+//run it three times
+
+//let lapsCompleted = 0
+
+//function incrementLap() {
+    //lapsCompleted = lapsCompleted + 1
+    
+//}
+
+
+//incrementLap()
+//incrementLap()
+//incrementLap()
+
 
