e3ee1e9f18444710a7b5e7eb60bd43158fa1ce23
Heather <hflanders.prsvr@gmail.com>
2024-04-23 20:04:26 -0500

    Complete

---
+++ note.js
@@ -0,0 +1,60 @@
+// let username = "per"
+// let message = "You have three new notifications"
+// console.log(message + ", " +  username + "!")
+
+// let myName = "Heather"
+// let greeting = "Hi, my name is "
+// let myGreeting = myName + greeting
+// console.log(myGreeting)
+
+// let points = 4
+// let bonusPoints = "10"
+// let totalPoints = points + bonusPoints
+
+// console.log(4 + 5)
+// console.log("2" + "4")
+// console.log("5" + 1)
+// console.log(100 + "100")
+
+// let welcomeEl = document.getElementById("welcome-el")
+// let myName = "Heather"
+// let greeting = "Hi, my name is "
+
+// welcomeEl.innerText = greeting + myName
+
+//welcomeEl.innerText = welcomeEl.innerText + "emoji"
+// do this instead: welcomeEl.innerText += "👌"
+
+
+//Finished APP
+// let saveEl = document.getElementById("save-el")
+// let countEl = document.getElementById("count-el")
+// let count = 0
+
+// function increment() {
+//     count += 1
+//     countEl.textContent  = count
+// }
+
+// function save() {
+//     let countStr = count + " - "
+//     saveEl.textContent += countStr
+//     countEl.textContent = 0
+//     count = 0
+// }
+
+// let firstName = "Heather"
+// let lastName = "Flanders"
+// let fullName = firstName + lastName
+
+// console.log(fullName)
+
+
+
+// let woman = "Linda"
+// let greeting = "Hi there"
+
+// function greetLinda() {
+//     console.log(greeting + woman)
+// }
+
--- script.js
+++ script.js
@@ -1,47 +1,16 @@
-// intialize the count as 0
-// listen for clicks on the increment button
-// incremement the count variable when the button is clicked
-// change the count-el in the HTML to reflect the new count
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
 
+function increment() {
+    count += 1
+    countEl.textContent  = count
+}
 
-// let countEl = document.getElementById("count-el")
-// let count = 0
-
-// function incrementCount() {
-//     count = count + 1
-//     countEl.innerText = count
-// }
-
-
-// function save() {
-//      console.log(count)
-// }
-
-// save()
-
-
-// let username = "per"
-// let message = "You have three new notifications"
-
-// console.log(message + ", " +  username + "!")
-
-// let myName = "Heather"
-// let greeting = "Hi, my name is "
-// let myGreeting = myName + greeting
-// console.log(myGreeting)
-
-// let points = 4
-// let bonusPoints = "10"
-// let totalPoints = points + bonusPoints
-
-// console.log(4 + 5)
-// console.log("2" + "4")
-// console.log("5" + 1)
-// console.log(100 + "100")
-
-// let welcomeEl = document.getElementById("welcome-el")
-// let myName = "Heather"
-// let greeting = "Hi, my name is "
-
-// welcomeEl.innerText = greeting + myName
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
 
